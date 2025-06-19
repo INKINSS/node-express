551d355eb67e6d026616c2a9f5966f89068f3fcd
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-06-18 21:01:50 -0600

    3.4: Backend de la Agenda Telefónica, paso 4

--- src/controllers/persons.controller.js
+++ src/controllers/persons.controller.js
@@ -3,3 +3,16 @@ import data from '../data/data.json' with { type : 'json' }
 export const getAllPersons = (req, res) => {
     res.send(data)
 }
+
+export const deletePerson = (req, res) => {
+    try {
+        const { id } = req.params
+        const person = data.filter(person => person.id !== Number(id))
+        if(!person) {
+            return res.status(404).json({ message: 'Person not found' })
+        }
+        res.send(person)
+    } catch (error) {
+        console.log(error)
+    }
+}
--- src/routes/persons.routes.js
+++ src/routes/persons.routes.js
@@ -1,7 +1,8 @@
 import express from 'express'
 const route = express.Router()
-import { getAllPersons } from '../controllers/persons.controller.js'
+import { deletePerson, getAllPersons } from '../controllers/persons.controller.js'
 
 route.get('/api/persons', getAllPersons)
+route.delete('/api/persons/:id', deletePerson)
 
 export default route
