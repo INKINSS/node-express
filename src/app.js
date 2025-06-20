a00e2d449818df9e1964ce0f62e1ec7977c0de71
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-06-20 07:14:35 -0600

    feat: añade cors y lo ejecuta como middleware

--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
 import express from 'express'
 import routes from './routes/index.js'
 import morganBody from 'morgan-body'
-// import { shotMethod } from './middlewares/method.middleware.js'
-// import { pageNotFound } from './middlewares/pageNotFound.middleware.js'
-
+import cors from 'cors'
 const app = express()
 
 
 //middlewares
 app.use(express.json())
 morganBody(app)
-// app.use(shotMethod)
-
-//error handlers
-// app.use(pageNotFound)
-
+app.use(cors())
 //routes
 app.use(routes)
 
