ed097bf4c35c5055ab890e65e0adf5574034d83f
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-06-21 15:17:43 -0600

    refactor: remove persons controller, routes, and method middleware

--- src/middlewares/method.middleware.js
+++
@@ -1,7 +0,0 @@
-export const shotMethod = (req, res, next) => {
-    console.log('method', req.method)
-    console.log('path', req.path)
-    console.log('body', req.body)
-    console.log('___')
-    next()
-}
