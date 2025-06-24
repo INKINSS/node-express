6f8e8bb6fcd00a63ec6d6aa378a10189faeca6d6
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-06-24 07:47:02 -0600

    feat: add middleware to validator name

---
+++ src/middlewares/persons.middleware.js
@@ -0,0 +1,8 @@
+//validar que el nombre tenga al menos 3 caracteres
+export const validateName = (req, res, next) => {
+    const { name } = req.body
+    if(name.length < 3) {
+        return res.status(400).json({ message: 'el nombre debe tener al menos 3 caracteres' })
+    }
+    next()
+}
