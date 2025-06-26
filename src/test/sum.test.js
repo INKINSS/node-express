eff448b7d6a93119eae6693b4a146cbedd703635
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-06-26 17:15:23 -0600

    4.4: Funciones Auxiliares y Pruebas Unitarias, paso 2

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,9 +1,23 @@
 /* global describe, test, expect */
-const { dummy } = require('../utils/test/dummy')
+const { dummy, totalLikes } = require('../utils/test/dummy')
 
 describe('dummy', () => {
     test('dummy returns one', () => {
         const result = dummy([])
         expect(result).toBe(1)
+    })
+})
+
+describe('total likes', () => {
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = totalLikes([{
+            _id: '5a422aa71b54a676234d17f7',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }])
+        expect(result).toBe(5)
     })
 })
