54248d3c5f46dfa20025fb4b6323844da1738cfc
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-06-26 17:26:05 -0600

    4.5*: Funciones Auxiliares y Pruebas Unitarias, paso 3

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,5 +1,5 @@
 /* global describe, test, expect */
-const { dummy, totalLikes } = require('../utils/test/dummy')
+const { dummy, totalLikes, favoriteBlog } = require('../utils/test/dummy')
 
 describe('dummy', () => {
     test('dummy returns one', () => {
@@ -19,5 +19,34 @@ describe('total likes', () => {
             __v: 0
         }])
         expect(result).toBe(5)
+    })
+})
+
+describe('favorite blog', () => {
+    test('should return the blog with the most likes', () => {
+        const result = favoriteBlog([{
+            _id: '5a422aa71b54a676234d17f7',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful 2',
+            author: 'Edsger W. Dijkstra 2',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html 2',
+            likes: 10,
+            __v: 0
+        }])
+        expect(result).toEqual({
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful 2',
+            author: 'Edsger W. Dijkstra 2',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html 2',
+            likes: 10,
+            __v: 0
+        })
     })
 })
