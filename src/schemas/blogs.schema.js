cea8f073161557ad50a5895fb53f449dfc318268
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-07-23 10:44:46 -0600

    feat: añade el tipo usuario al blogSchema
    configura para elimianr el campo _v0 y cambia el nombre del schema

--- src/schemas/blogs.schema.js
+++ src/schemas/blogs.schema.js
@@ -16,9 +16,19 @@ export const blogSchema = new Schema({
     likes: {
         type: Number,
         default: 0
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     versionKey: false
+}).set('toJSON', {
+    transform: (_doc, ret) => {
+      ret.id = ret._id.toString()
+      delete ret._id
+      delete ret.__v
+    }
 })
 
-export const Blog = model('Blog', blogSchema, 'blogs')
+export const Blog = model('Blog', blogSchema, 'notes')
