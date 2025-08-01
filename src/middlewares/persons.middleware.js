8ef7c1ee1f80180b6c24d88d4c809fde9bab16db
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-01 09:33:07 -0600

    feat: añade middlware de validarNombre como campo unico en rutas de usuarios

--- src/middlewares/persons.middleware.js
+++ src/middlewares/persons.middleware.js
@@ -1,6 +1,6 @@
 export const validateName = (req, res, next) => {
     const { name } = req.body
-    if(name.length < 3) {
+    if(name.lenght < 3) {
         return res.status(400).json({ message: 'el nombre debe tener al menos 3 caracteres' })
     }
     next()
--- src/routes/persons.routes.js
+++ src/routes/persons.routes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const route = express.Router()
 import { deletePerson, getAllPersons, getPersonById, createPerson, updatePerson } from '../controllers/persons.controller.js'
-import { validateName, validateNumber } from '../middlewares/persons.middleware.js'
+import { validateName } from '../middlewares/persons.middleware.js'
 
 route.get('/api/persons', getAllPersons)
 route.get('/api/persons/:id', getPersonById)
-route.post('/api/persons', validateName, validateNumber, createPerson)
+route.post('/api/persons', validateName, createPerson)
 route.put('/api/persons/:id', updatePerson)
 route.delete('/api/persons/:id', deletePerson)
 
