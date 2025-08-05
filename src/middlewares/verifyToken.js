c79e4cdf06da4aaefc86929d821b964c16285451
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-05 09:56:15 -0600

    feat: añade logica de verificar token como middlware y la añade a la ruta de crear blogs

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+
+export const verifyToken = (req, _res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+        req.user = decodedToken
+        next()
+    } catch (error) {
+        if(error.name === 'TokenExpiredError') {
+            return next(new Error('Token expired'))
+        }
+        next(error)
+    }
+}
--- src/routes/blogs.routes.js
+++ src/routes/blogs.routes.js
@@ -2,9 +2,10 @@ import express from 'express'
 const route = express.Router()
 
 import { createBlog, getAllBlogs, getBlogById, updateBlog, deleteBlog } from '../controllers/blogs.controller.js'
+import { verifyToken } from '../middlewares/verifyToken.js'
 
 route.get('/api/blogs', getAllBlogs)
-route.post('/api/blogs', createBlog)
+route.post('/api/blogs', verifyToken, createBlog)
 route.get('/api/blogs/:id', getBlogById)
 route.put('/api/blogs/:id', updateBlog)
 route.delete('/api/blogs/:id', deleteBlog)
