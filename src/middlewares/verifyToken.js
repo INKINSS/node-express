3df0633f166cd32c0266d848f80e5bf561a5d056
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-05 10:45:54 -0600

    refactor: mejora el mensaje para el usuario si no existe token

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,15 +1,19 @@
 import jwt from 'jsonwebtoken'
 
 //verify token como middleware
-export const verifyToken = (req, _res, next) => {
+export const verifyToken = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1]
+        const authHeader = req.headers.authorization
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({ message: 'Token no encontrado' })
+        }
+        const token = authHeader.split(' ')[1]
         const decodedToken = jwt.verify(token, process.env.SECRET)
         req.user = decodedToken
         next()
     } catch (error) {
         if(error.name === 'TokenExpiredError') {
-            return next(new Error('Token expired'))
+            return res.status(401).json({ message: 'Token expirado' })
         }
         next(error)
     }
