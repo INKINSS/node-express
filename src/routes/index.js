a64509d3a6eb0cb061ee3ca51dbc56336a0cd260
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-13 09:50:33 -0600

    feat: añade la ruta en el barrel de exportacion de rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import infoRoutes from "./info.routes.js";
 import personsRoutes from './persons.routes.js'
 import blogsRoutes from './blogs.routes.js'
 import loginRoutes from './login.js'
+import routerContacts from './contact.routes.js'
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use(infoRoutes)
 router.use(personsRoutes)
 router.use(blogsRoutes)
 router.use(loginRoutes)
+router.use(routerContacts)
 
 export default router
